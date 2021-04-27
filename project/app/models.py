986c721242f1e97543aeaa18bd40034e6963277c
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-04-27 21:04:24 +0200

    Añado tablas para organizar la estructura de datos #2

--- project/app/models.py
+++ project/app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-import requests as rq
-
+import requests 
+import json
 
 #help: https://stackoverflow.com/questions/58558989/what-does-djangos-property-do
 class request(models.Model):
@@ -9,31 +9,49 @@ class request(models.Model):
     PartToaddToBaseUrl=models.CharField(max_length=200)
     funcToExtractDataFromJsonName=models.CharField(max_length=100)
     ParamsOrDataDictStructure=models.JSONField()
+    headers=models.JSONField(null=True)
 
     class Suit(models.TextChoices):
         GET = 'GET'
         POST = 'POST'
      
 
-    typeRequests = models.CharField(choices=Suit.choices)
-
-    
+    typeRequests = models.CharField(choices=Suit.choices,max_length=5)
 
     @property
-    def getJson(self,baseUrl,listParamsValues):
+    def getJson(self,baseUrl,listParamsValues,typeOfData=""):
         structureWithValues=dict(self.ParamsOrDataDictStructure)
         contIndex=0
         for key in structureWithValues.keys():
             structureWithValues[key]=listParamsValues[contIndex]
             contIndex+=1
-        if typeRequests=="GET":
+
+        if self.typeRequests=="GET":
+            return requests.get(baseUrl+self.PartToaddToBaseUrl,params=structureWithValues)
         else:
-            
-        
-    
+            if typeOfData=='str':
+                structureWithValues=json.dumps(structureWithValues)
+
+            return requests.post(baseUrl+self.PartToaddToBaseUrl,data=structureWithValues,headers=dict(self.headers))
+
+
     @property
-    def executeFunction(self,listParamsValues):
-        return exec(self.funcToExtractDataFromJsonName+'('+'self.fillParamsOrDataDictStructure'+'('+'listParamsValues'+')'+')')
+    def executeFunction(self):
+        json=self.getJson()
+        return exec(self.funcToExtractDataFromJsonName+'(json)')
+
+
+class RESTApi(models.Model):
+    BaseUrl=models.CharField(max_length=50)
+    APIKey=models.CharField(max_length=50)
+    Requests = models.ForeignKey(request, on_delete=models.CASCADE)
+    
+  
+
+
+
+
+    
         
 
 
