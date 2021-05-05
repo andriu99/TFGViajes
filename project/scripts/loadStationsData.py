d04fc2d4a3c8a77c8890648ecce950b1580b5830
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-06 00:06:52 +0200

    Añado el script para cargar datos de estaciones #3 #11

--- project/scripts/loadStationsData.py
+++ project/scripts/loadStationsData.py
@@ -1,45 +1,38 @@
 import csv
 import requests
 import urllib.parse
-from app.models import Node
-
- trainlineRESTApi=RESTApi.objects.get(name='TrainlineRESTApi') 
-
-geoCatalogRequests=Request.objects.get(name='getSkyscannerGeocatalog')
-    getTokenRequest=Request.objects.get(name='getTokenSkyscanner')
+from app.models import Node,Request,RESTApi
 
 
+def run():
+    trainlineRESTApi=RESTApi.objects.get(name='TrainlineRESTApi') 
+    getStationInformation=Request.objects.get(name='getStationInformationTrainline')
 
-# # Se crea un objeto que abre el fichero y representa su contenido
-csvfile = open('stationsData.txt', 'r',encoding='utf-8')
-# # Se crea un objeto que formatea el contenido como CSV
-reader = csv.reader(csvfile, delimiter=';')
+    # # Se crea un objeto que abre el fichero y representa su contenido
+    csvfile = open('stationsData.txt', 'r',encoding='utf-8')
+    # # Se crea un objeto que formatea el contenido como CSV
+    reader = csv.reader(csvfile, delimiter=';')
 
-
-# # Se accede a cada registro del fichero
-for fila in reader:
-    if fila[8]=='ES':
-            
-        try:
-            latitude=fila[5]
-            longitude=fila[6]
-            name=fila[1]
-            Id=fila[0]
-            if latitude=='' or longitude=='' or name=='' or Id=='':
-                url='https://www.trainline.eu/api/v5/stations?context=search'
-                url+='&q={Place}'.format(Place=urllib.parse.quote(fila[1]))
-                postform=requests.get(url)
-                jsonEstaciones=postform.json()
-                Id=(jsonEstaciones['stations'][0]['id'])
-                latitude=(jsonEstaciones['stations'][0]['latitude'])
-                longitude=(jsonEstaciones['stations'][0]['longitude'])
-            
-            new_node=Node(name,Id,latitude,longitude)
-            new_node.save()
+    # # Se accede a cada registro del fichero
+    for fila in reader:
+        if fila[8]=='ES':
                 
-        except:
-            print("Error en: "+fila[1])
+            try:
+                latitude=fila[5]
+                longitude=fila[6]
+                name=fila[1]
+                Id=fila[0]
+                if latitude=='' or longitude=='' or name=='' or Id=='':
+                    jsonEstaciones=getStationInformation(trainlineRESTApi.BaseUrl,'{Place}'.format(Place=urllib.parse.quote(fila[1])))
+                    Id=jsonEstaciones['stations'][0]['id']
+                    latitude=jsonEstaciones['stations'][0]['latitude']
+                    longitude=jsonEstaciones['stations'][0]['longitude']
+                
+                new_node=Node(name,Id,float(latitude),float(longitude))
+                new_node.save()
+                    
+            except:
+                print("Error en: "+fila[1])
 
-
-# # Se cierra el fichero de lectura
-csvfile.close()
+    # # Se cierra el fichero de lectura
+    csvfile.close()
