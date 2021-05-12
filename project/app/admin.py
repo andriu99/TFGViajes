8ec08cfb23f54586c6ffefaf32419c39a3a56763
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-12 13:02:16 +0200

    Cumplo #35 y #36 y avanzo en #27

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Request,RESTApi,Node
+from .models import Request,RESTApi,Node,Trip,skyscannerTrip,blablaTrip,busOrTrainTrip
 
 admin.site.register(Request)
 admin.site.register(RESTApi)
 admin.site.register(Node)
+admin.site.register(Trip)
+admin.site.register(skyscannerTrip)
+admin.site.register(blablaTrip)
+admin.site.register(busOrTrainTrip)
+
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import userRequest
-from app.models import Request,RESTApi
+from app.models import Request,Trip
 import datetime
 
 
@@ -8,9 +8,9 @@ def home(request):
     if request.method == 'POST':
         form = userRequest(request.POST)
         if form.is_valid():
+            Trip.objects.all().delete()
             getBlablaCarTrips=Request.objects.get(name='getBlablaCarTrips')
-            # print(form.cleaned_data['date'])
-            # print(form.cleaned_data['date']+datetime.timedelta(days=1))
+          
             originalDate=form.cleaned_data['date']
             start_date_local=datetime.datetime(originalDate.year,originalDate.month,originalDate.day)
             start_date_local_string=start_date_local.isoformat()
@@ -22,14 +22,25 @@ def home(request):
             end_coordinates=str(form.cleaned_data['lat_Dest'])+','+str(form.cleaned_data['lon_Dest'])
             iterableBlablaCar=getBlablaCarTrips.executeFunction([getBlablaCarTrips.RApi.APIKey,start_coordinates,end_coordinates,'EUR',start_date_local_string,end_date_local_string])
             for (url,
-                 startData_date,startData_city,startData_address,startData_latitude,startData_longitude,
-                 endDatadate,endDatacity,endDataaddress,endDatalatitude,endDatalongitude,
+                 startData_str,startData_city,startData_address,startData_latitude,startData_longitude,
+                 endData_str,endDatacity,endDataaddress,endDatalatitude,endDatalongitude,
                  price
             ) in iterableBlablaCar:
-                print(url)
-                print(startData_date,startData_city,startData_address,startData_latitude,startData_longitude)
-                print(endDatadate,endDatacity,endDataaddress,endDatalatitude,endDatalongitude)
+                startData_date = datetime.datetime.strptime(startData_str, '%Y-%m-%dT%H:%M:%S')
+                endData_date = datetime.datetime.strptime(endData_str, '%Y-%m-%dT%H:%M:%S')
+
+                diffDate=endData_date-startData_date
+                duration=diffDate.seconds
+                print(duration)
                 print(price)
+                print(type(price))
+                #new_trip=Trip(departureDate=startData_date,arrivalDate=endData_date,duration=duration,price=)
+
+
+                # print(url)
+                # print(startData_date,startData_city,startData_address,startData_latitude,startData_longitude)
+                # print(endData_date,endDatacity,endDataaddress,endDatalatitude,endDatalongitude)
+                # print(price)
     else:
         form = userRequest()
     
