ade28dc490e66ec018c10a0c06df07f5a3962e10
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-13 20:29:32 +0200

    Soluciono #43

--- project/app/views.py
+++ project/app/views.py
@@ -33,16 +33,21 @@ def home(request):
                  price
 
             ) in iterableBlablaCar:
-            
+
                 startData_date = parseStrDate(startData_str,startData_latitude,startData_longitude) 
                 endData_date = parseStrDate(endData_str,endData_latitude,endData_longitude)
 
-                diffDate=endData_date-startData_date
-                duration=diffDate.seconds
+                
+                difHours_departure_withUT0=int(startData_date.isoformat()[-6]+startData_date.isoformat()[-4]) #diference between 00:00 time zone and departure time zone
+                difHours_arrival_withUT0=int(startData_date.isoformat()[-6]+endData_date.isoformat()[-4])
+                
+                difHours_arrival_departure=difHours_arrival_withUT0-difHours_departure_withUT0
 
+                duration=endData_date-startData_date
+                duration=duration+timedelta(hours=(-1)*difHours_arrival_departure)
                 
              
-                new_trip=Trip(departureDate=startData_date,arrivalDate=endData_date,duration=duration,price=float(price))
+                new_trip=Trip(departureDate=startData_date,arrivalDate=endData_date,duration=duration.seconds,price=float(price))
                 new_trip.save()
 
                 new_blablatrip=blablaTrip(link=url,
@@ -55,38 +60,39 @@ def home(request):
 
 
             
-            # getskyscannerTrips=Request.objects.get(name='getTokenSkyscanner')
-            # token=getskyscannerTrips.executeFunction([getskyscannerTrips.RApi.APIKey])
+            getskyscannerTrips=Request.objects.get(name='getTokenSkyscanner')
+            token=getskyscannerTrips.executeFunction([getskyscannerTrips.RApi.APIKey])
 
-            # getSessionKeySkyscanner=Request.objects.get(name='getSessionKeySkyscanner')
-            # getFlightsInformationSkyscanner=Request.objects.get(name='getFlightsInformationSkyscanner')
+            getSessionKeySkyscanner=Request.objects.get(name='getSessionKeySkyscanner')
+            getFlightsInformationSkyscanner=Request.objects.get(name='getFlightsInformationSkyscanner')
             
-            # outboundDate='{y}-{m}-{d}'.format(y=str(start_date_local.year),m=str(start_date_local.month).zfill(2),d=str(start_date_local.day).zfill(2))
+            outboundDate='{y}-{m}-{d}'.format(y=str(start_date_local.year),m=str(start_date_local.month).zfill(2),d=str(start_date_local.day).zfill(2))
             
-            # filter_DepartureNodes=filterAirportsNodes(start_coordinates)
-            # filter_ArrivalNodes=filterAirportsNodes(end_coordinates)
-            # for departureNode in filter_DepartureNodes:
-            #     for arrivalNode in filter_ArrivalNodes:
-            #         paramsList=['Economy','ES','EUR','es-ES','iata',departureNode.code,arrivalNode.code,outboundDate,1,0,0,token]
-            #         SessionKey=getSessionKeySkyscanner.executeFunction(paramsList)
-            #         getFlightsInformationSkyscanner.PartToaddToBaseUrl+=SessionKey
-            #         results=getFlightsInformationSkyscanner.executeFunction([0,token])
+            filter_DepartureNodes=filterAirportsNodes(start_coordinates)
+            filter_ArrivalNodes=filterAirportsNodes(end_coordinates)
+            for departureNode in filter_DepartureNodes:
+                for arrivalNode in filter_ArrivalNodes:
+                    paramsList=['Economy','ES','EUR','es-ES','iata',departureNode.code,arrivalNode.code,outboundDate,1,0,0,token]
+                    SessionKey=getSessionKeySkyscanner.executeFunction(paramsList)
+                    getFlightsInformationSkyscanner.PartToaddToBaseUrl+=SessionKey
+                    results=getFlightsInformationSkyscanner.executeFunction([0,token])
 
 
-            #         urlList=(getFlightsInformationSkyscanner.PartToaddToBaseUrl.split('/'))
-            #         urlList.pop()
-            #         getFlightsInformationSkyscanner.PartToaddToBaseUrl="/".join(urlList)
+                    urlList=(getFlightsInformationSkyscanner.PartToaddToBaseUrl.split('/'))
+                    urlList.pop()
+                    getFlightsInformationSkyscanner.PartToaddToBaseUrl="/".join(urlList)
 
 
-            #         for price,urlPay,departure_date,arrival_date,duration,airlineName,airlineUrlImage in results:
-            #             startData_date = parseStrDate(departure_date,departureNode.latitude,departureNode.longitude)
+                    for price,urlPay,departure_date,arrival_date,duration,airlineName,airlineUrlImage in results:
+                        startData_date = parseStrDate(departure_date,departureNode.latitude,departureNode.longitude)
 
-            #             endData_date = parseStrDate(arrival_date,arrivalNode.latitude,arrivalNode.longitude)
+                        endData_date = parseStrDate(arrival_date,arrivalNode.latitude,arrivalNode.longitude)
+                       
 
-            #             new_trip=Trip(departureDate=startData_date,arrivalDate=endData_date,duration=duration,price=float(price))
-            #             new_trip.save()
-            #             new_skyscannerTrip=skyscannerTrip(urlPay=urlPay,airlineName=airlineName,airlineUrlImage=airlineUrlImage,trip=new_trip)
-            #             new_skyscannerTrip.save()
+                        new_trip=Trip(departureDate=startData_date,arrivalDate=endData_date,duration=duration*60,price=float(price))
+                        new_trip.save()
+                        new_skyscannerTrip=skyscannerTrip(urlPay=urlPay,airlineName=airlineName,airlineUrlImage=airlineUrlImage,trip=new_trip)
+                        new_skyscannerTrip.save()
 
 
 
