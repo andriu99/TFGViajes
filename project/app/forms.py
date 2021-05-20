9e80ae4625d436070774d304bba1b34ccb079338
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-20 22:16:58 +0200

    Añado un id a los atributos de latitud y longitud para poder usarlos en la parte de javascript

--- project/app/forms.py
+++ project/app/forms.py
@@ -5,10 +5,10 @@ class DateInput(forms.DateTimeInput):
     input_type='date'
     
 class userRequest(forms.Form):
-    lat_Origin = forms.FloatField(widget=forms.HiddenInput(),required=False)
-    lon_Origin = forms.FloatField(widget=forms.HiddenInput(),required=False)
-    lat_Dest = forms.FloatField(widget=forms.HiddenInput(),required=False)
-    lon_Dest = forms.FloatField(widget=forms.HiddenInput(),required=False)
+    lat_Origin = forms.FloatField(widget=forms.HiddenInput(attrs={"id":"latO"}),required=False,)
+    lon_Origin = forms.FloatField(widget=forms.HiddenInput(attrs={"id":"lonO"}),required=False)
+    lat_Dest = forms.FloatField(widget=forms.HiddenInput(attrs={"id":"latD"}),required=False)
+    lon_Dest = forms.FloatField(widget=forms.HiddenInput(attrs={"id":"lonD"}),required=False)
     
     def __init__(self, *args, **kwargs):
         super(userRequest,self).__init__(*args, **kwargs)
