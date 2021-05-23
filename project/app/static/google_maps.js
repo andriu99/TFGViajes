763ea3c6cc1f7bbd493ef5318607284c06ca270e
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-23 17:13:45 +0200

    Consigo #67

--- project/app/static/google_maps.js
+++ project/app/static/google_maps.js
@@ -69,6 +69,7 @@ function initMap(){
         })
 
     }else{
+        get_currentLocation(map)
         map.setCenter({
             lat : 40.4165, 
             lng : -3.70256
@@ -77,20 +78,61 @@ function initMap(){
     
 
 
-
-
-        
-    
-    
-   
-
-    
-
-    
-    
-
-    
 }
+
+function get_currentLocation(map){
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          current_lat=position.coords.latitude;
+          current_lng=position.coords.longitude;
+
+          const pos = {
+            lat: current_lat,
+            lng: current_lng,
+          };
+          map.setCenter({
+            lat : current_lat, 
+            lng : current_lng
+        });
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+// function get_address_withLocation(){
+
+
+// }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+}
+
 
 function autocomplete_id(idname){
 
