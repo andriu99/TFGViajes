f24c0773cd750166fc8eea7b1a0d9c9d6c25a81d
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-25 18:05:51 +0200

    Quito campos innecesarios del formulario #67

--- project/app/forms.py
+++ project/app/forms.py
@@ -13,11 +13,6 @@ class userRequest(forms.Form):
     lat_Dest = forms.FloatField(widget=forms.HiddenInput(attrs={"id":"lat_Dest"}),required=False,initial=181)
     lon_Dest = forms.FloatField(widget=forms.HiddenInput(attrs={"id":"lon_Dest"}),required=False,initial=181)
 
-    lat_currentLocat= forms.FloatField(widget=forms.HiddenInput(attrs={"id":"lat_currentLocat"}),required=False,initial=181) 
-    lon_currentLocat= forms.FloatField(widget=forms.HiddenInput(attrs={"id":"lon_currentLocat"}),required=False,initial=181)
-
-
-    current_address=forms.CharField(max_length=200,widget=forms.HiddenInput(attrs={"class":"data","id":"current_address"}),initial='.')
 
     origin_address=forms.CharField(max_length=200,widget=forms.TextInput(attrs={"class":"data","id":"origin_address"}))
     destination_address=forms.CharField(max_length=200,widget=forms.TextInput(attrs={"class":"data","id":"destination_address"}))
