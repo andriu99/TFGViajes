465bc228628ccdded2b57ffaa661b257429769bd
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-05-31 17:43:06 +0200

    Soluciono #109

--- project/app/scheduler/scheduler.py
+++ project/app/scheduler/scheduler.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from django_apscheduler.models import DjangoJobExecution
 import sys
 
+from pytz import HOUR
+
 # This is the function you want to schedule - add as many as you want and then register them in the start() function below
 def deactivate_expired_accounts():
     today = timezone.now()
@@ -16,7 +18,7 @@ def start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
     # run this job every 24 hours
-    scheduler.add_job(deactivate_expired_accounts, 'interval', minutes=1, name='clean_accounts', jobstore='default')
+    scheduler.add_job(deactivate_expired_accounts, 'interval', HOUR=1, name='clean_accounts', jobstore='default')
     register_events(scheduler)
     scheduler.start()
     print("Scheduler started...", file=sys.stdout)
