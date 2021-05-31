5eafc2875667df7e3a50b9b0111f47fa6c886010
Andrés Rodríguez <arr1006@alu.ubu.es>
2021-06-01 00:27:09 +0200

    #113 completado

--- prototipoInicial/prueba.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime as dt, timedelta 
-
-
-from datetime import timezone
-now = dt.now(timezone.utc)
-now1 = dt.now(timezone.utc)+timedelta(days=2)
-
-print(now<now1)
-
--- prototipoInicial/pruebas.py
+++
@@ -1,2 +0,0 @@
-import datetime
-print(datetime.date(2021,5,15)+datetime.timedelta(days=1))
